ee6631dcebfe1be134f9e95eb7d4c3cba8dbe6d8
sharmaanand086 <38725515+sharmaanand086@users.noreply.github.com>
2019-03-01 15:11:43 +0530

    Create pdfcontent.php

---
+++ pdfcontent.php
@@ -0,0 +1,78 @@
+ 
+<?php
+
+
+ $type9 ='<table id="mainTable" style="width: 730px;border:none;" cellspacing="0">
+    	<tbody>
+        <tr style="background:#e4e4e4;width:730px;height: 100px;margin:30px 0px;" >
+                <td><p style="width: 90%;font-weight:800;padding-left:20px;margin:0px;padding-top:30px;padding-bottom:10px;font-family: Tahoma, Arial, Verdana,sans-serif;font-size: 18px;color:#ff6f0d;text-transform:uppercase;" ></p> 
+    				<p style="width: 90%;font-weight:100;letter-spacing:0.5px;line-height:18px;font-family: Tahoma, Arial, Verdana,sans-serif;margin:0px;padding-bottom:30px;padding-left:20px;padding-top:20px;font-size: 14px;color:#000;">Mediators believe that to be loved and valued, you must blend in and go with the flow.
+Consequently, Mediators seek harmony and are inclusive, amiable, easygoing, comfortable and
+steady; they also can be self-forgetting, conflict-avoidant and stubborn.</p> 
+                </td>
+        </tr>
+  </tbody>
+  </table>
+  <table id="mainTable" style="width: 800px;">
+    	<tbody>
+            <tr>
+    			<td width="50%" style="font-family: Tahoma, Arial, Verdana,sans-serif;font-size: 12px;color: #000000;font-weight:800;">
+    			<p style="color:#ff6f0d; padding-bottom:0px;width: 90%;font-weight:800;font-family: Tahoma, Arial, Verdana,sans-serif;font-size: 18px;margin-bottom:0;padding-left:20px;" >At your best state:</p> 	
+<p style=" width: 90%;font-weight:100;letter-spacing:0.5px;line-height:18px;font-family: Tahoma, Arial, Verdana,sans-serif;font-size: 13px;color:#000;padding-left:20px;">You are like the 14th Dalai Lama*, are fully alive, vital and awake to themselves and others in each moment anew.  They are independent yet interdependent, indomitable yet yielding, inwardly strong yet resilient.  They are self-possessed, alert, exuberant and joyful.  With their natural gifts and talents the universe tends to move them in the direction of bringing people together, working for peace and healing the world.  Being fully present to themselves, others and the world around them, very healthy Nines are able to create mutually satisfying 
+relationships with everyone.</p>
+<p style=" width: 90%;font-weight:100;letter-spacing:0.5px;line-height:18px;font-family: Tahoma, Arial, Verdana,sans-serif;font-size: 13px;color:#000;padding-left:20px;">* My speculation based on his life and my experience with the Enneagram</p>
+            
+
+<p style="color:#ff6f0d; width: 90%;font-weight:800;font-family: Tahoma, Arial, Verdana,sans-serif;font-size: 18px;margin-bottom:0;padding-left:20px;padding-top:15px;" >At your good state: </p>
+<p style=" width: 90%;font-weight:100;letter-spacing:0.5px;line-height:18px;font-family: Tahoma, Arial, Verdana,sans-serif;font-size: 13px;color:#000;padding-left:20px;">
+You are calm, receptive, adaptable, compassionate, patient and nonjudgmental.  They accept others as they are and are able to create peace and harmony wherever they go.  Healthy Nines are genuinely easygoing and good-natured.  People are able to relax around healthy Nines because healthy Nines are relaxed.  They are also optimistic, unpretentious and unself-conscious.  There is an innocence and simplicity about healthy Nines that is so endearing.  They are emotionally stable and trusting of themselves and others.  Their openness to life and ability to go with the flow allows them to experience a deep sense of inner peace and serenity.  Healthy Nines are excellent, uncritical listeners and great at understanding differences in viewpoints between other people.  In fact, they are often called “the mediator” because they are so good at unbiasedly seeing different points of view.  Healthy Nines have an innate gift for comforting and reassuring others.  They make for steady, supportive friends who are always willing to lend a helping hand when asked.  They constantly look for elements of 
+commonality that can bring people together.  They exert a subtle healing and stabilizing influence on others during difficult times.</p> 
+
+</td>
+</tr>
+<tr>
+<td>
+
+<p style=" width: 90%;font-weight:100;letter-spacing:0.5px;line-height:18px;font-family: Tahoma, Arial, Verdana,sans-serif;font-size: 13px;color:#000;padding-left:20px;">
+Healthy Nines are not self-centered but they have a healthy sense of self and are able to set and accomplish goals as well as prioritize their time.  Healthy Nines are usually serene and peaceful but can be powerful, dynamic and even assertive if the situation calls for it.
+Healthy Nines are highly imaginative, creative and good communicators.  They often make excellent mediators, negotiators, counselors, storytellers, arbitrators and diplomats.  Healthy Nines often inspire others with their quiet strength, equanimity and genuine contentment with their life.</p> 
+
+
+
+<p style="color:#ff6f0d; width: 90%;font-weight:800;font-family: Tahoma, Arial, Verdana,sans-serif;font-size: 18px;margin-bottom:0;padding-left:20px;padding-top:15px;" >At your average state:</p> 
+
+<p style=" width: 90%;font-weight:100;letter-spacing:0.5px;line-height:18px;font-family: Tahoma, Arial, Verdana,sans-serif;font-size: 13px;color:#000;padding-left:20px;">You tend to be out of touch with their own needs and priorities.  They see themselves as modest people who prefer to stay in the background and go along with the wishes of others.  Being inconspicuous and unassuming allows average Nines to minimize their expectations of others (and themselves) so they will not be frustrated or disappointed when someone lets them down.  By being inconspicuous and blending into the background they are also less likely to be intruded upon.  These are subconscious defenses to help them achieve their Unconscious Desire – to keep their peace of mind.  Unfortunately, being self-effacing, accommodating and agreeable has its downside.  Sometimes Nines are overlooked, or worse, taken advantage of.  Average Nines don’t like that but it’s hard for them to stand up for themselves because that might cause a conflict and then they’ll lose their peace of mind!  So, average Nines tend to just go along with requests made of them because they like the feeling of being connected with others and because they don’t like conflict.  And it’s easier to just go along with others; less energy expenditure.  Inwardly though they can remain stubbornly resistant if it was something they didn’t really want to do but just agreed to do out of habit.  Then they can become passive-aggressive and silently defiant (like saying they will do something and then just not doing it or doing it annoyingly slowly or just “forgetting” to do it).  In short, average Nines try to avoid conflict as much as possible because it causes them a great deal of anxiety (and loss of peace of mind).  Average Nines like predictability and don’t like too much change. 
+They highly value a harmonious and comfortable life with fairly regular routines and soothing habits.</p> 
+            
+<p style=" width: 90%;font-weight:100;letter-spacing:0.5px;line-height:18px;font-family: Tahoma, Arial, Verdana,sans-serif;font-size: 13px;color:#000;padding-left:20px;">Some average Nines are extremely hard working and/or physically active but they are spiritually lazy about living in alignment with their own highest values and priorities.  Average Nines are famous for being indecisive, undisciplined and for procrastinating.  That’s because they don’t really know what they want and what’s most important to do first!  They can fritter the day away doing mindless busywork or familiar routines (like watching TV, playing on the computer, 
+playing video games, eating, daydreaming, etc.) or tasks that are unimportant.  </p>
+
+<p style=" width: 90%;font-weight:100;letter-spacing:0.5px;line-height:18px;font-family: Tahoma, Arial, Verdana,sans-serif;font-size: 13px;color:#000;padding-left:20px;">In relationships it is easy for Nines to idealize their partner because they see the best in him/her and like feeling merged.  One of the reasons average Nines are so calm is because they are out of touch with their own lively, dynamic, instinctive energy.  Thus, average Nines are often attracted to energetic people and, since they are out of touch with their own needs, they can live vicariously through their partner’s desires and priorities rather than their own.  It’s as if they go on “automatic pilot” to themselves.  Curiously, average Nines like the feeling of a deep connection with their partner but they will resist if there is any kind of pressure put on them to change, conform, shape up, be more..... or be any other way than they are.  Other ways average Nines try to stay comfortable and maintain their peace of mind are by: being passive; tuning others out; spacing out and daydreaming; being oblivious to what’s going on around them; withdrawing from conflicts; and sweeping problems under the rug (not dealing with them and hoping they will go away on their own).  Average Nines want life to be smooth and easy but their peace of mind is really nothing more than an avoidance of problems and anything upsetting.  If average Nines aren’t careful they can find
+that problems have accumulated out of control and then they could deteriorate into the unhealthy category.</p>
+
+
+</p>
+</td>
+</tr>
+<tr>
+<td>
+
+<p style="color:#ff6f0d; width: 90%;font-weight:800;font-family: Tahoma, Arial, Verdana,sans-serif;font-size: 18px;margin-bottom:0;padding-left:20px;padding-top:15px;" >At your below average state:</p> 
+<p style=" width: 90%;font-weight:100;letter-spacing:0.5px;line-height:18px;font-family: Tahoma, Arial, Verdana,sans-serif;font-size: 13px;color:#000;padding-left:20px;">You neglect their mounting problems and become extremely irresponsible, repressed, apathetic, listless and ineffectual.  They defend their story that everything is okay but they know very well it is not.  They placate others to get them off their back (and keep their peace of mind) but there is a lot of suppressed anger brewing just below the surface of their calm.  Occasionally they do erupt into short bursts of temper or even fits of rage.  They are resigned, spiritually dead inside and drearily go through life feeling as though there is nothing they can do about their situation.  
+They fantasize about someone magically rescuing them from all their problems.
+
+ 
+
+<p style="color:#ff6f0d; width: 90%;font-weight:800;font-family: Tahoma, Arial, Verdana,sans-serif;font-size: 18px;margin-bottom:0;padding-left:20px;padding-top:10px;" >Destructive:</p>
+
+<p style=" width: 90%;font-weight:100;letter-spacing:0.5px;line-height:18px;font-family: Tahoma, Arial, Verdana,sans-serif;font-size: 13px;color:#000;padding-left:20px;">You try to block out of their awareness anything that might affect them.  They are extremely fearful and anxious about all the neglected problems in their life and feel completely incapable of facing them let alone handling them.  They are so emotionally and physically drained from the denial of their problems that they are totally neglectful of themselves and others.  They can barely deal with reality at all.  Well-intentioned people who try to help the destructive Nine are met with hostility and perceived as a threat to the Nine’s illusion that everything will work itself out.  At worst, they become severely depressed, 
+dissociating themselves from any kind of conflict and unable to function.  They can become completely shut-down and suicidal.</p> 
+
+</td>
+<tr>
+
+        </tbody>
+         </table>' ;
+         
+         
+         ?>
